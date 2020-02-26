303b2922568361a84354df5c21a4e74b0455d65a
Rafal Magda <rafalmag@gmail.com>
2020-02-26 21:54:10 +0100

    v0.1 testing

--- README.md
+++ README.md
@@ -19,9 +19,21 @@ https://www.sparkfun.com/products/14478
 
 https://github.com/sparkfun/MaKeyMaKey
 
+https://www.instructables.com/community/How-does-Makey-Makey-sense-touch/
+
+https://www.youtube.com/watch?v=gTU9NunYVnM
 
 ## Arduino Pro Micro (32u4) pinout
 
 ![Arduino Pro Micro](arduino_pro_micro.jpg)
 
 ![Arduino Pro Micro](Arduino-pro-micro-5V-16MHz.jpg)
+
+
+## makey makey use cases inspiration
+
+https://makeymakey.com/blogs/how-to-instructions/apps-for-plug-and-play
+
+https://www.instructables.com/id/Makey-Makey-2/
+
+https://makeymakey.com/blogs/how-to-instructions/getting-started-with-scratch
--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,44 @@
 #include "Mouse.h"
 
 // Keyboard keyboard;
+int value = 0;
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
   // put your setup code here, to run once:
-  Keyboard.begin();
-  Mouse.begin();
+  // Keyboard.begin();
+  // Mouse.begin();
+
+  /* Set up input pins 
+   DEactivate the internal pull-ups, since we're using external resistors */
+  pinMode(A0, INPUT);
+  digitalWrite(A0, LOW);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+boolean low = false;
+boolean logged = false;
+
+void loop()
+{
+  value = analogRead(A0);
+  if (value < 1000)
+  {
+    low = true;
+    if (!logged)
+    {
+      Serial.print("Val A0: ");
+      Serial.println(value);
+      Serial.print("Volt : ");
+      Serial.println(map(value, 0, 1023, 0, 5));
+      Serial.println("---");
+      logged = true;
+    }
+  }
+  else
+  {
+    low = false;
+    logged = false;
+  }
+  delay(10);
 }
